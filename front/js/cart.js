55cec04825431e6d9bfc799e2e2c6c0cd4e67d8a
dkartes <hetyei.kevin@gmail.com>
2022-07-28 18:41:17 +0200

    ajout script cart avec l'API

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,31 +1,69 @@
-let basketParse = JSON.parse(localStorage.getItem("basket"));
-let basket = window.localStorage.basket;
-// Presence du panier dans le local Storage ? //
-console.log(basket);
+let basketKey = window.localStorage.basket;
+const idUrl = new URL(window.location.href).searchParams.get("id");
+
+// Presence du panier dans le local Storage + appel de l'api ? //
 function showBasket() {
-  if (basket) {
-    basketParse;
+  if (basketKey) {
+    fetch("http://localhost:3000/api/products")
+      .then(res => res.json())
+      .then(objetProduits => {
+        getBasket(objetProduits);
+        console.log(objetProduits);
+      })
+      .catch(err => {
+        console.log(err);
+      });
   } else {
     let titleCart = document.querySelector("h1");
-
     titleCart.textContent += " est vide !";
     console.log(titleCart);
   }
 }
 
-showBasket();
-/*let basket = [];
-if ((basketGet = null)) {
-  let titleCart = document.querySelector("h1");
-  //console.log(titleCart);
-  titleCart.textcontent = "Votre panier est vide !";
-  console.log(basketGet);
-} else {
-  basketParse;
-  return basketParse;
+function getBasket(index) {
+  let basket = JSON.parse(localStorage.getItem("basket"));
+  if (basket.length != 0) {
+    for (let choice of basket) {
+      console.log(choice);
+      for (let i = 0, h = index.length; i < h; i++) {
+        if (choice.idUrl === index[i]._id) {
+          // création et ajout de valeurs à panier qui vont servir pour les valeurs dataset
+          choice.name = index[i].name;
+          choice.price = index[i].price;
+          choice.image = index[i].imageUrl;
+          choice.description = index[i].description;
+          choice.alt = index[i].altTxt;
+          // creation de l'id article //
+          let cartItem = document.getElementById("cart__items");
+          let cartArticle = document.createElement("article");
+          cartItem.appendChild(cartArticle);
+          cartArticle.className = "cart__item";
+          cartArticle.setAttribute("data-id", choice.idUrl);
+          cartArticle.setAttribute("data-color", choice.color);
+          // création image //
+          let imgCreate = document.createElement("img");
+          let div = document.createElement("div");
+          cartArticle.appendChild(div);
+          div.className = "cart__item__img";
+          div.appendChild(imgCreate);
+          imgCreate.setAttribute("src", choice.image);
+          imgCreate.setAttribute("alt", choice.alt);
+          // création du content de l'article //
+          let h2Content = document.createElement("h2");
+          let colorContent = document.createElement("p");
+          let cartContent = document.querySelector(".cart__item__content");
+          div;
+          (div.setAttribute = "class"), cart__item__content;
+          div.appendChild(h2Content).textContent = choice.name;
+          div.appendChild(colorContent).textContent = choice.color;
+          div.appendChild(colorContent).textContent = choice.price;
+        }
+      }
+    }
+  }
 }
-//}
 
+showBasket();
 /*async function createBasketCart() {
   await showBasket();
   if (basket) {
@@ -33,13 +71,7 @@ if ((basketGet = null)) {
       await fetch("http://localhost:3000/api/products/" + basket[i].idUrl)
         .then(res => res.json())
         .then(article => {
-          // creation de l'article //
-          let cartItem = document.getElementById("cart__items");
-          let cartArticle = document.createElement("article");
-          cartItem.appendChild(cartArticle);
-          cartArticle.className = "cart__item";
-          cartArticle.setAttribute("data-id", basket[i].idUrl);
-          cartArticle.setAttribute("data-color", basket[i].color);
+        
 
           // création image //
           let imgCreate = document.createElement("img");
