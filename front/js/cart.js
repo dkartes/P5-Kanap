edfe7a017ad7ae4a1d075f8ba57df53c619a4c72
dkartes <hetyei.kevin@gmail.com>
2022-07-29 12:03:35 +0200

    Finalisation de la première partie de la carte du panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -33,6 +33,7 @@ function getBasket(index) {
           choice.image = index[i].imageUrl;
           choice.description = index[i].description;
           choice.alt = index[i].altTxt;
+
           // creation de l'id article //
           let cartItem = document.getElementById("cart__items");
           let cartArticle = document.createElement("article");
@@ -40,6 +41,7 @@ function getBasket(index) {
           cartArticle.className = "cart__item";
           cartArticle.setAttribute("data-id", choice.idUrl);
           cartArticle.setAttribute("data-color", choice.color);
+
           // création image //
           let imgCreate = document.createElement("img");
           let div = document.createElement("div");
@@ -48,15 +50,46 @@ function getBasket(index) {
           div.appendChild(imgCreate);
           imgCreate.setAttribute("src", choice.image);
           imgCreate.setAttribute("alt", choice.alt);
+
           // création du content de l'article //
+          let div2 = document.createElement("div");
+          let div3 = document.createElement("div");
           let h2Content = document.createElement("h2");
           let colorContent = document.createElement("p");
+          let priceContent = document.createElement("p");
           let cartContent = document.querySelector(".cart__item__content");
-          div;
-          (div.setAttribute = "class"), cart__item__content;
-          div.appendChild(h2Content).textContent = choice.name;
-          div.appendChild(colorContent).textContent = choice.color;
-          div.appendChild(colorContent).textContent = choice.price;
+          cartArticle.appendChild(div2);
+          div2.className = "cart__item__content";
+          div2.appendChild(div3);
+          div3.className = "cart__item__content__description";
+          div3.appendChild(h2Content).textContent = choice.name;
+          div3.appendChild(colorContent).textContent = choice.color;
+          div3.appendChild(priceContent).textContent = choice.price;
+
+          // création des settings //
+          let div4 = document.createElement("div");
+          let div5 = document.createElement("div");
+          let qtyContent = document.createElement("p");
+          let inputContent = document.createElement("input");
+          let div6 = document.createElement("div");
+          let deletItem = document.createElement("p");
+
+          div2.appendChild(div4);
+          div4.className = "cart__item__content__settings";
+          div4.appendChild(div5);
+          div5.className = "cart__item__content__settings__quantity";
+          div5.appendChild(qtyContent);
+          div5.appendChild(inputContent);
+          inputContent.setAttribute("type", "number");
+          inputContent.className = "itemQuantity";
+          inputContent.setAttribute("name", "itemQuantity");
+          inputContent.setAttribute("min", "1");
+          inputContent.setAttribute("max", "100");
+          inputContent.setAttribute("value", choice.quantity);
+          div4.appendChild(div6);
+          div6.className = "cart__item__content__settings__delete";
+          div6.appendChild(deletItem).textContent = "Supprimer";
+          deletItem.className = "deleteItem";
         }
       }
     }
